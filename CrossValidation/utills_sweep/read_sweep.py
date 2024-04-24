0c500678c6846df2b357dbc6ea362ed0b95608c1
4oli <h>
2024-04-24 09:52:01 +0200

    compared sweeps "paper_11"

--- CrossValidation/utills_sweep/read_sweep.py
+++ CrossValidation/utills_sweep/read_sweep.py
@@ -1,95 +1,91 @@
 import wandb
 import pandas as pd
 import json
+import shutil
 
 
-def format_methods_from_list(methods_list):
-    if methods_list == 'None' or methods_list == 'none':
-        return 'None'
+class SweepReader:
+    def __init__(self, sweep_id, project_path="daswoldemar/bachelor"):
+        self.sweep_id = sweep_id
+        self.project_path = project_path
+        self.dataframe = None
 
-    if isinstance(methods_list, str):
-        return methods_list
+    def fetch_sweep_runs(self) -> pd.DataFrame:
+        # Initialize a client
+        api = wandb.Api()
 
-    # move to string
-    methods_str = str(methods_list)
-    # remove brackets
-    methods_str = methods_str[1:-1]
+        # Fetch the sweep
+        sweep = api.sweep(f"{self.project_path}/{self.sweep_id}")
 
-    # "'," -> "\n"
-    methods_str = methods_str.replace("', '", "\n")
-    # "'" -> ""
-    methods_str = methods_str.replace("'", "")
+        # List to hold data for each run
+        data = []
 
-    return methods_str
+        # Iterate through all runs in the sweep
+        for run in sweep.runs:
+            try:
+                path = api.artifact(name=f"{self.project_path}/run-{run.id}-test_confusion_matrix:v0").download()
 
-# Initialize wandb
-# Make sure you're logged in. You might need to do `wandb login` in your terminal.
+                with open(f'artifacts/run-{run.id}-test_confusion_matrix:v0/test_confusion_matrix.table.json',
+                          'r') as f:
+                    json_data = json.load(f)
 
-def fetch_sweep_runs(sweep_id, project_path):
-    # Initialize a client
-    api = wandb.Api()
+                columns_json = json_data['columns']
+                data_json = json_data['data'][0]
 
-    # Fetch the sweep
-    sweep = api.sweep(f"{project_path}/{sweep_id}")
+                # Fetch desired details from the run. Adjust these fields based on your needs.
+                run_data = {
+                    'Run ID': run.id,
+                    "Transform Init": self.format_methods_from_list(run.config.get("init_transform")),
+                    'Transforms': self.format_methods_from_list(run.config.get("transform")),
+                    'Output Transform': self.format_methods_from_list(run.config.get("output_transform")),
+                    "Fold": int(run.config.get("fold")[-1]),
+                    "Best Epoch": run.summary.get("best_val_loss_epoch"),
+                    'Test F1': run.summary.get('test_f1'),
+                    'Test Precision': run.summary.get('test_precision'),
+                    'Test Recall': run.summary.get('test_recall'),
+                    'Test Accuracy': run.summary.get('test_accuracy'),
+                    f"{columns_json[0]}": data_json[0],
+                    f"{columns_json[1]}": data_json[1],
+                    f"{columns_json[2]}": data_json[2],
+                    f"{columns_json[3]}": data_json[3],
+                }
+                data.append(run_data)
+            except:
+                print(f"Run {run.id} has no test_confusion_matrix artifact")
 
-    # List to hold data for each run
-    data = []
+        shutil.rmtree('artifacts')
 
-    # Iterate through all runs in the sweep
-    for run in sweep.runs:
-        try:
-            path = api.artifact(name=f"daswoldemar/bachelor/run-{run.id}-test_confusion_matrix:v0").download()
+        self.dataframe = pd.DataFrame(data)
 
-            with open(f'artifacts/run-{run.id}-test_confusion_matrix:v0/test_confusion_matrix.table.json', 'r') as f:
-                json_data = json.load(f)
+        # Convert to DataFrame
+        return pd.DataFrame(data)
 
-            columns_json = json_data['columns']
-            data_json = json_data['data'][0]
+    def get_dataframe(self) -> pd.DataFrame:
+        return self.dataframe
 
-            # Fetch desired details from the run. Adjust these fields based on your needs.
-            run_data = {
-                'Run ID': run.id,
-                "Transform Init": format_methods_from_list(run.config.get("init_transform")),
-                'Transforms': format_methods_from_list(run.config.get("transform")),
-                'Output Transform': format_methods_from_list(run.config.get("output_transform")),
-                "Fold": int(run.config.get("fold")[-1]),
-                "Best Epoch": run.summary.get("best_val_loss_epoch"),
-                'Test F1': run.summary.get('test_f1'),
-                'Test Precision': run.summary.get('test_precision'),
-                'Test Recall': run.summary.get('test_recall'),
-                'Test Accuracy': run.summary.get('test_accuracy'),
-                f"{columns_json[0]}": data_json[0],
-                f"{columns_json[1]}": data_json[1],
-                f"{columns_json[2]}": data_json[2],
-                f"{columns_json[3]}": data_json[3],
-            }
-            data.append(run_data)
-        except:
-            print(f"Run {run.id} has no test_confusion_matrix artifact")
+    def read_from_csv(self, filename: str) -> pd.DataFrame:
+        self.dataframe = pd.read_csv(filename)
+        return self.dataframe
 
-    # Convert to DataFrame
-    df = pd.DataFrame(data)
+    def save_to_csv(self, filename: str) -> None:
+        self.dataframe.to_csv(filename, index=False)
+        print("Data saved to CSV file")
 
-    # delete artifacts folder
-    import shutil
-    shutil.rmtree('artifacts')
+    def format_methods_from_list(self, methods_list: list or str) -> str:
+        if methods_list == 'None' or methods_list == 'none':
+            return 'None'
 
-    return df
+        if isinstance(methods_list, str):
+            return methods_list
 
+        # move to string
+        methods_str = str(methods_list)
+        # remove brackets
+        methods_str = methods_str[1:-1]
 
-# Replace 'your_sweep_id' and 'your_project_path' with your specific details
-sweep_id = 'torch'
-project_path = 'daswoldemar/bachelor'
+        # "'," -> "\n"
+        methods_str = methods_str.replace("', '", "\n")
+        # "'" -> ""
+        methods_str = methods_str.replace("'", "")
 
-# Fetch data
-df = fetch_sweep_runs(sweep_id, project_path)
-
-# Save to Excel
-#excel_filename = f"{sweep_id}_runs.xlsx"
-#df.to_excel(excel_filename, index=False)
-
-# Save to CSV
-csv_filename = f"{sweep_id}_runs.csv"
-df.to_csv(csv_filename, index=False)
-
-print("Data saved to CSV file")
+        return methods_str
