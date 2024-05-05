afe393eb48f095cd38c32c3250a757c13afae203
4oli <h>
2024-05-05 20:38:58 +0200

    added sweep for combined by fold

--- CrossValidation/lists_to_conf.py
+++ CrossValidation/lists_to_conf.py
@@ -73,7 +73,7 @@ def update_yaml_transforms(transforms, base_yaml_path, new_yaml_path):
 
 
 base_yaml_file_path = 'sweep_gpu.yaml'
-file_with_transforms = 'sweep_pixel-level_single.py'
+file_with_transforms = 'sweep_combined_fold.py'
 
 # Assume the file 'script.py' contains the definitions of the transformations
 transforms = read_transforms(file_with_transforms)
